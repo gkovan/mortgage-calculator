055b1e01f19bcc0359b19efb7d55ebedba24d894
Gerry Kovan <gkovan@amazon.com>
2023-02-19 20:47:42 -0500

    remove unused import

--- src/test/java/com/gk/mortgage/calculator/task/MortgageProcessorTaskImplTest.java
+++ src/test/java/com/gk/mortgage/calculator/task/MortgageProcessorTaskImplTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gk.mortgage.calculator.MortgageCalculatorApplication;
 import com.gk.mortgage.calculator.domain.MortgageCalculatorRequest;
