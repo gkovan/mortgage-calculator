c7f92d6f132330239c9d8243fcc965ca88892ab0
Gerry Kovan <gkovan@amazon.com>
2023-03-05 10:58:48 -0500

    remove unused imports

--- src/test/java/com/gk/mortgage/calculator/application/HealthEndpointTest.java
+++ src/test/java/com/gk/mortgage/calculator/application/HealthEndpointTest.java
@@ -1,16 +1,12 @@
 package com.gk.mortgage.calculator.application;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HealthEndpointTest {
