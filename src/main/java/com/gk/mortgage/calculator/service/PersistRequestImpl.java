d06ffcef960b9eb82a0faf01c94218bec92ae707
Gerry Kovan <gkovan@amazon.com>
2023-03-13 10:55:44 -0400

    externalize dynamoDB database name

--- src/main/java/com/gk/mortgage/calculator/service/PersistRequestImpl.java
+++ src/main/java/com/gk/mortgage/calculator/service/PersistRequestImpl.java
@@ -1,9 +1,11 @@
 package com.gk.mortgage.calculator.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.gk.mortgage.calculator.domain.MortgageCalculatorDataToPersist;
 
+import lombok.Setter;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
@@ -13,21 +15,25 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @Service
 public class PersistRequestImpl implements PersistRequest {
 
-    private final DynamoDbClient ddb = DynamoDbClient.builder()
-            .region(Region.US_EAST_1)
-            .build();
-    private final DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(ddb)
-            .build();
+        @Setter
+        @Value("${environment.dynamoDBTable}")
+        private String dynamoDBTable;
 
-    private final String flightsTableName = "mortgage-calculator-dev";
+        private final DynamoDbClient ddb = DynamoDbClient.builder()
+                        .region(Region.US_EAST_1)
+                        .build();
+        private final DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                        .dynamoDbClient(ddb)
+                        .build();
 
-    public String saveToDB(MortgageCalculatorDataToPersist requestDataToPersist) {
-        DynamoDbTable<MortgageCalculatorDataToPersist> mappedTable = enhancedClient
-                .table(flightsTableName, TableSchema.fromBean(MortgageCalculatorDataToPersist.class));
+        // private final String flightsTableName = "mortgage-calculator-dev";
 
-        mappedTable.putItem(requestDataToPersist);
+        public String saveToDB(MortgageCalculatorDataToPersist requestDataToPersist) {
+                DynamoDbTable<MortgageCalculatorDataToPersist> mappedTable = enhancedClient
+                                .table(dynamoDBTable, TableSchema.fromBean(MortgageCalculatorDataToPersist.class));
 
-        return requestDataToPersist.getTimestamp();
-    }
+                mappedTable.putItem(requestDataToPersist);
+
+                return requestDataToPersist.getTimestamp();
+        }
 }
